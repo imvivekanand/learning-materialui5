afb62f2dad30eacef5599fe4f09267ee561c346a
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-03-14 15:59:45 +0530

    Button Group

--- src/components/MuiButtons.tsx
+++ src/components/MuiButtons.tsx
@@ -1,35 +1,77 @@
-import {Stack, Button, IconButton} from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
+import { Stack, Button, IconButton, ButtonGroup } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
 export const MuiButtons = () => {
   return (
     <Stack spacing={4}>
-    <Stack spacing={2} direction='row'>
-      <Button variant='text' href='https://google.com'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secodary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("Sent")}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup
+          variant="contained"
+          color="success"
+          orientation="vertical"
+          size="small"
+          aria-label="alignment button group"
+        >
+          <Button onClick={() => alert("Left Click")}>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
     </Stack>
-    <Stack spacing={2} direction='row'>
-      <Button variant='contained' color='primary'>Primary</Button>
-      <Button variant='contained' color='secondary'>Secodary</Button>
-      <Button variant='contained' color='error'>Error</Button>
-      <Button variant='contained' color='warning'>Warning</Button>
-      <Button variant='contained' color='info'>Info</Button>
-      <Button variant='contained' color='success'>Success</Button>
-    </Stack>
-    <Stack display='block' spacing={2} direction='row'>
-      <Button variant='contained' size='small'>Small</Button>
-      <Button variant='contained' size='medium'>Medium</Button>
-      <Button variant='contained' size='large'>Large</Button>
-    </Stack>
-    <Stack spacing={2} direction='row'>
-    <Button variant='contained' startIcon={<SendIcon/>} disableRipple onClick={() => alert("Sent")}>Send</Button>
-    <Button variant='contained' endIcon={<SendIcon/>} disableElevation>Send</Button>
-    
-    <IconButton aria-label='send' color='success' size='small'>
-      <SendIcon/>
-    </IconButton>
-    </Stack>
-    </Stack>
-  )
-}
+  );
+};
